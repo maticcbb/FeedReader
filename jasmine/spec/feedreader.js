730ec3fa658d824986f95bdcec914253308ae9d0
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2018-06-21 19:43:17 +0200

    Feed loading tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,12 +8,13 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-$(function() {
+$(function () {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
+    describe('RSS Feeds',  () => {
+
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,16 +22,16 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-           var feedCheck,
-               menuTest; 
+        
+        var feedCheck,
+            menuTest;
 
-         beforeEach(function() 
-         {
-           feedCheck = new FeedsCheck();
-           menuTest = new MenuCheck();
-         });
+        beforeEach( () => {
+            feedCheck = new FeedsCheck();
+            menuTest = new MenuCheck();
+        });
 
-        it('are defined', function() {
+        it('are defined',() => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -39,31 +40,54 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('all URLs defined and checked', function () {
+
+        it('all URLs defined and checked',() => {
             feedCheck.urlCheck(allFeeds);
             expect(feedCheck.checked_url).toBe(true);
-            
+
         })
 
-        it('all name defined and checked ', function () {
+         /* TODO: Write a test that loops through each feed
+          * in the allFeeds object and ensures it has a name defined
+          * and that the name is not empty.
+          */
+
+        it('all name defined and checked ',() => {
             feedCheck.nameCheck(allFeeds);
             expect(feedCheck.checked_name).toBe(true);
         })
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+       
     });
 
-        
+
+    
+
+    
+
+
+   
     /* TODO: Write a new test suite named "The menu" */
+
+    describe('The menu',() => {
+
+        var menuTest;
+
+        beforeEach(() => {
+
+            menuTest = new MenuCheck();
+        });
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        
+
+        it('menu is hidden by default',() => {
+            menuTest.menuVisibility();
+            expect(menuTest.menu_checked).toBe(true);
+
+        })
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
@@ -71,45 +95,66 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
 
-          describe('The menu', function () {
+        it('menu toggle is working properly', () => {
+            menuTest.toggle();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
 
-              var menuTest;
+             menuTest.toggle();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        })
 
-              beforeEach(function () {
+    });
 
-                  menuTest = new MenuCheck();
-              });
+    /* TODO: Write a new test suite named "Initial  Entries" */
 
-              it('menu is hidden by default', function () {
-                  menuTest.menuVisibility();
-                  expect(menuTest.menu_checked).toBe(true);
+    
+    describe('Initial Entries' , () => {
+ 
+        beforeEach((done) => { 
+            loadFeed(0, () => {
+            done();
+        });
+    });
 
-              })
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
 
-              it('menu toggle is working properly' , function () {
-                 
-                  menuTest.menuHide();
-                  expect(menuTest.hidden_checked).toBe(true);
-                  
-                  menuTest.menuShow();
-                  expect(menuTest.visible_checked).toBe(true);
-              })
+        it( 'a single .entry element within the.feed container after loadFeed() is called' ,  ((done) => {
+            var numberEntries = document.querySelector('.feed').getElementsByClassName('entry').length;
+            expect(numberEntries).toBeGreaterThan(0);
+            done();
+        }));
 
-          })
+    })
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection' , () => {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+        var initFeedSelection;
+        beforeEach((done) => {
+            loadFeed(0,() => {
+                initFeedSelection = document.querySelector('.feed').innerHTML;
+
+                loadFeed(1, () => {
+                    done();
+                });
+            });
+        });
+    /* TODO: Write a test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
+        it('loadFeed function content changes' , (done) => {
+            var newFeedSelection = document.querySelector('.feed').innerHTML;
+            expect(initFeedSelection).not.toBe(newFeedSelection);
+            done();
+        });
+
+    });
+    
 }());
--- src/FeedsCheck.js
+++ src/FeedsCheck.js
@@ -1,21 +1,21 @@
-function FeedsCheck () {
-this.table_url = [] ;
-this.checked_url = false;
-this.table_name = [] ;
-this.checked_name = false;
+function FeedsCheck() {
+    this.table_url = [];
+    this.checked_url = false;
+    this.table_name = [];
+    this.checked_name = false;
 }
 
 FeedsCheck.prototype.urlCheck = function (allFeeds) {
-  for (let i = 0; i < allFeeds.length; i++) {
-      if (allFeeds[i].url !== '' && allFeeds[i].hasOwnProperty('url')) {
-          this.table_url.push('OK');
-          if (this.table_url.length === allFeeds.length) {
-              this.checked_url = true;
-              
-          }
-      }
+    for (let i = 0; i < allFeeds.length; i++) {
+        if (allFeeds[i].url !== '' && allFeeds[i].hasOwnProperty('url')) {
+            this.table_url.push('OK');
+            if (this.table_url.length === allFeeds.length) {
+                this.checked_url = true;
 
-  }
+            }
+        }
+
+    }
 }
 
 FeedsCheck.prototype.nameCheck = function (allFeeds) {
@@ -24,7 +24,7 @@ FeedsCheck.prototype.nameCheck = function (allFeeds) {
             this.table_name.push('OK');
             if (this.table_name.length === allFeeds.length) {
                 this.checked_name = true;
-                
+
             }
         }
 
